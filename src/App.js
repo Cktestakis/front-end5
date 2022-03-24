1a225e3340c64cb486d64049ea4c760f500eeb15
Cody Ktestakis <64503311+Cktestakis@users.noreply.github.com>
2022-03-24 13:33:04 -0400

    App.js updated Creating out app.js and import saved books, SavedBooks and Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import "./styles.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  ApollowClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
