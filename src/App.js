844ea9ca210c2d1ad9f3a3159f51243c1139eb7f
Cody Ktestakis <64503311+Cktestakis@users.noreply.github.com>
2022-03-24 13:46:17 -0400

    Styles css updated to App.css Going off of recent applications created to create Navbar for this exercise.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import "./styles.css";
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import {
-  ApollowClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-
-import SearchBooks from "./pages/SearchBooks";
-import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
+
+import "./styles/App.css";
 
 export default function App() {
   return (
