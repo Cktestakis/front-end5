9d48f66840de82c8a3ba275fc5eeb0ef86b8e96d
Cody Ktestakis <64503311+Cktestakis@users.noreply.github.com>
2022-03-24 13:57:41 -0400

    App.js updated Added Route for navbar but unable to remove the errors.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import Navbar from "./components/Navbar";
 
 import "./styles/App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </Router>
+
+      <Footer />
+    </>
   );
 }
